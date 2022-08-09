31204a22a80e2f7ef5881e453c8a00fdb6c7615a
ipekozbay <20060401@stu.omu.edu.tr>
2022-08-09 14:33:18 +0300

    09.08

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from "react";
-
 import './App.css';
 import {BrowserRouter } from 'react-router-dom';
-import {Switch} from 'react-router'
 import { Route } from 'react-router-dom';
-
-
 import Navbar from './components/Navbar/Navbar';
 import User from './components/User/User';
 import Home from './components/Home/Home';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navbar> </Navbar>
-        <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/users/:userId" component={User}></Route>
-        </Switch>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { useState } from "react";
-
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
