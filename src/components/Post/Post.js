fad4256f702820e3d14e28c3eb67f0ad13f17bf2
ipekozbay <20060401@stu.omu.edu.tr>
2022-08-15 16:34:17 +0300

    The database connection of the like object has been made

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -34,9 +34,10 @@ export default function Post(props) {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [commentList, setCommentList] = useState([]);
-    const [isLiked, setIsLiked ]= useState(false);
+    const [isLiked, setIsLiked] = useState(false);
     const isInitialMount = useRef(true);
     const [likeCount, setLikeCount] = useState(likes.length);
+    const [likeId,setLikeId] =useState(null);
 
     const handleExpandClick = () => {
         setExpanded(!expanded);
@@ -46,11 +47,15 @@ export default function Post(props) {
 
     const handleLike = () => {
         setIsLiked(!isLiked);
-        
-        if(!isLiked)
+
+        if (!isLiked){
+            saveLike();
             setLikeCount(likeCount + 1);
-        else
+        }
+        else{
+            deleteLike();
             setLikeCount(likeCount - 1);
+        }
     };
 
     const refreshComments = () => {
@@ -71,11 +76,45 @@ export default function Post(props) {
             )
     }
 
-    const checkLikes = () => {
-        var likeControl = likes.find((like =>like.userId === userId))
-        if(likeControl != null)
-            setIsLiked(true);
+    const saveLike = () => {
+        fetch("http://localhost:8080/likes/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application.json",
+            },
+            body: JSON.stringify({
+                postId: postId,
+                userId: userId,
+            }),
+        })
+            .then((res) => res.json())
+            .catch((err) => console.log(err))
     }
+    
+    const deleteLike = () => {
+        fetch("http://localhost:8080/likes/" + likeId, {
+            method :"DELETE",
+        })
+        .catch((err) => console.log(err))
+    }
+
+    // const checkLikes = () => {
+    //     var likeControl = likes.find((like => like.userId === userId))
+    //     if (likeControl != null)
+    //         setLikeId(likeControl.id)
+    //         setIsLiked(true);
+    // }
+
+    const checkLikes = () => {
+        var likeControl = likes.find((like =>  ""+like.userId === localStorage.getItem("currentUser")));
+        if(likeControl != null){
+          setLikeId(likeControl.id);
+          setIsLiked(true);
+        }
+      }
+
+      useEffect(() => {checkLikes()},[])
+
     // useEffect(() => {
     //     if (isInitialMount.current)
     //          isInitialMount.current = false
@@ -83,7 +122,7 @@ export default function Post(props) {
     //          refreshComments();
     // },[]);
 
-    useEffect (( )=> {checkLikes()},[])
+    useEffect(() => { checkLikes() }, [])
 
     return (
         <div>
@@ -135,7 +174,7 @@ export default function Post(props) {
                         {error ? "error" :
                             isLoaded ? commentList.map(comment => (
                                 <div key={comment.id}>
-                                    <Comment  userId={1} userName={"username1"} text={comment.text} key={comment.id}></Comment>
+                                    <Comment userId={1} userName={"username1"} text={comment.text} key={comment.id}></Comment>
                                 </div>
                             )) : "loading"}
                         <CommentForm refreshComments={refreshComments} userId={1} userName={"username1"} postId={postId}>
