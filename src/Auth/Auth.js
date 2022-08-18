e4ceadcaea5a4fbc66d2388522f64c2bad5ea89a
ipekozbay <20060401@stu.omu.edu.tr>
2022-08-18 16:18:25 +0300

    18.08.2022

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,3 +1,4 @@
+import { History } from "@mui/icons-material";
 import { FormControl, Input, InputLabel, Button, FormHelperText } from "@mui/material";
 import React, { useState } from "react";
 
@@ -6,18 +7,58 @@ export default function Auth() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleUsername = (value) => {
+        setUsername(value);
+    }
+
+    const handlePassword = (value) => {
+        setPassword(value);
+    }
+
+    const sendRequest = (path) => {
+        fetch("http://localhost:8080/auth/" + path, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        })
+            .then((res) => res.json())
+            .then((result) => {localStorage.setItem("tokenKey", result.message);
+                              localStorage.setItem("currentUser", result.userId);
+                              localStorage.setItem("userName", result.username)})
+            .catch((err) => console.log(err))
+    }
+
+    const handleRegister = (value) => {
+        sendRequest("register");
+        setUsername("");
+        setPassword("");
+        window.history.go("http://localhost:8080/auth/");
+    }
+    const handleLogin = (value) => {
+        sendRequest("login");
+        setUsername("");
+        setPassword("");
+    }
+
     return (
         <FormControl>
             <InputLabel>username</InputLabel>
-            <Input />
+            <Input onChange={(i) => handleUsername(i.target.value)} />
             <InputLabel style={{ top: 80 }} >password</InputLabel>
-            <Input style={{ top: 30 }} />
+            <Input style={{ top: 30 }}
+                onChange={(i) => handlePassword(i.target.value)} />
             <Button variant="contained"
                 style={{
                     marginTop: 60,
                     backkground: "linear-gradient(45deg, #2196F3, #21CBF3 90%)",
                     color: "white"
-                }}> Register
+                }}
+                onClick={handleRegister}> Register
             </Button>
             <FormHelperText style={{ margin: 20 }}>
                 are you already registered?
@@ -26,7 +67,8 @@ export default function Auth() {
                 style={{
                     backkground: "linear-gradient(45deg, #2196F3, #21CBF3 90%)",
                     color: "white"
-                }}> Login
+                }}
+                onClick={handleLogin}> Login
             </Button>
         </FormControl>
     )
