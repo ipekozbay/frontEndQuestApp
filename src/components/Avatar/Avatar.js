c5a36984791d86f8a5ad588b2ed6c83991332c15
ipekozbay <20060401@stu.omu.edu.tr>
2022-08-31 14:45:36 +0300

    avatar created

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { useState } from "react";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function Avatar() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Card sx={{ maxWidth: 345, margin: 5 }}>
+                <CardMedia
+                    component="img"
+
+                    image="/avatars/avatar0.png"
+                    alt="user avatar"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        UserName
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        user info
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                </CardActions>
+                <Button onClick={handleOpen}>change avatar</Button>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Text in a modal
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Modal>
+            </Card>
+        </div>
+    )
+}
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {useParams} from "react-router-dom"
+import Avatar from "../Avatar/Avatar";
 
 function User(){
     const{userId} = useParams();
     return(
         <div>
             user {userId}
+            <Avatar/>
           </div>
     )
 }
