01eeede85cd04fb114f6a3aec3f52dcebfae32ab
ipekozbay <20060401@stu.omu.edu.tr>
2022-09-13 18:08:54 +0300

    user activity added

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {useParams} from "react-router-dom"
 import Avatar from "../Avatar/Avatar";
+import UserActivity from "../UserActivity/UserActivity";
 
 export default function User(){
     const{userId} = useParams();
     return(
-        <div>
-            user {userId}
-            <Avatar/>
+        <div style={{display :"flex"}}>
+            <Avatar avatarId={0}/>
+            <UserActivity/>
           </div>
     )
 }
