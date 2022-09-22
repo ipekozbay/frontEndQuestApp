88b1dc7cc99efedcf99345289b8e5b2f980698cb
ipekozbay <20060401@stu.omu.edu.tr>
2022-09-22 18:37:02 +0300

    dialog component added.

--- src/components/UserActivity/UserActivity.js
+++ src/components/UserActivity/UserActivity.js
@@ -3,13 +3,21 @@ import { useParams } from "react-router-dom";
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { useState } from "react";
 import { Button } from "@mui/material";
+//import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+import Post from "../Post/Post";
 
 const columns = [
     {
@@ -21,6 +29,83 @@ const columns = [
     },
 ];
 
+function PopUp(props) {
+    const [isOpen,postId]=props;
+    const [open, setOpen] = React.useState(false);
+    const [post,setPost] = useState(null);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const getPost =() => {
+        fetch("http://localhost:8080/posts" + postId, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("tokenKwy")
+            },
+        })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setPost(result);
+                },
+                (error) => {
+                    console.log(error)
+
+                }
+            )
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    useEffect(() =>{
+        setOpen(isOpen);
+    },[isOpen]);
+
+    useEffect(() =>{
+        getPost();
+    },[postId]);
+
+    return (
+        post != null ?
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Open full-screen dialog
+            </Button>
+            <Dialog
+                fullScreen
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Transition}
+            >
+                <AppBar sx={{ position: 'relative' }}>
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            onClick={handleClose}
+                            aria-label="close"
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                        <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+                            Sound
+                        </Typography>
+                        <Button autoFocus color="inherit" onClick={handleClose}>
+                            save
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <Post likes={post.postLikes} postId={post.id} userId={post.userId} 
+                    userName={post.userName} title={post.title} text={post.title} >
+                </Post>
+            </Dialog> : "loading"
+        </div>
+    )
+}
 export default function UserActivity(props) {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -28,9 +113,12 @@ export default function UserActivity(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [rows, setRows] = useState([]);
     const { userId } = props;
+    const [isOpen,setIsOpen] = useState(null);
+    const [selectedPost, setSelectedPost] = useState(null);
 
     const handleNotification = (postId) => {
-        
+        setSelectedPost(postId);
+        setIsOpen(true);
     };
 
     const getActivity = () => {
@@ -71,7 +159,7 @@ export default function UserActivity(props) {
 
     const { useId } = useParams();
     return (
-        <div>
+        <div><PopUp isOpen={isOpen} postId={selectedPost}/>
             <Paper sx={{ width: '100%', overflow: 'hidden' }}>
                 <TableContainer sx={{ maxHeight: 440 }}>
                     <Table stickyHeader aria-label="sticky table">
